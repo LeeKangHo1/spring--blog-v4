fc2ac3bf798648d6868d750982436c887d8f93d6
kangho.lee.pr <kangho.lee.pr@gmail.com>
2024-11-16 00:58:05 +0900

    간단한 수정

--- src/main/java/com/example/blog/board/BoardResponse.java
+++ src/main/java/com/example/blog/board/BoardResponse.java
@@ -3,9 +3,9 @@ package com.example.blog.board;
 import lombok.Data;
 
 public class BoardResponse {
-    // DTO 여러 개 관리하기 위해 클래스를 만들어서 만드는 것
+    // 여러 개의 DTO 객체를 관리하기 위해 BoardResponse 클래스 작성
 
-    @Data // 이건 Getter, Setter 필요
+    @Data // Getter, Setter 필요
     public static class DTO {
         // BoardResponse.DTO 로 쓰기 위해 static으로 올리는 것
         private int id;
